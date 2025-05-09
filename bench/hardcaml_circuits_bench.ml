9891f79d08b15f4265382ffa2f8b18a3d76fef1f
public-release <as-public-release@janestreet.com>
2025-05-09 20:36:12 +0100

    v0.18~preview.130.31+242

--- bench/hardcaml_circuits_bench.ml
+++ bench/hardcaml_circuits_bench.ml
@@ -1,6 +1,1 @@
-module Bench_lfsr = Bench_lfsr
-module Bench_mul = Bench_mul
-module Bench_prefix_sum = Bench_prefix_sum
-module Bench_rac = Bench_rac
-module Bench_sort = Bench_sort
-module Import = Import
+(* Nothing to export. *)
